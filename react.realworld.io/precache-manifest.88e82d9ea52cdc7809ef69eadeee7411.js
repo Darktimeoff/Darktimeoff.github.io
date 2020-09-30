8194d8a683a6f14032d55704a2cb1ead6f30ff0a
Jevhenii <alexkorolixinofficial@gmail.com>
2020-09-30 15:57:58 +0300

    /

---
+++ react.realworld.io/precache-manifest.88e82d9ea52cdc7809ef69eadeee7411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36aaec142e83f3159f88948407284bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b739f795bdee7e96b2ec",
+    "url": "/static/css/main.215bf16f.chunk.css"
+  },
+  {
+    "revision": "c7a2e7de32a9c71350f2",
+    "url": "/static/js/2.e2a603bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e2a603bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b739f795bdee7e96b2ec",
+    "url": "/static/js/main.9d7871a9.chunk.js"
+  },
+  {
+    "revision": "cff7b27e4e76655a1dfa",
+    "url": "/static/js/runtime-main.f62029ad.js"
+  }
+]);
